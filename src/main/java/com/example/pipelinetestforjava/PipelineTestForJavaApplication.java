73f697e1c29143feb0216224ff2244277e46b869
liyifeng <liyifeng@xlauncher.io>
2020-08-18 15:28:59 +0800

    add hello world

---
+++ src/main/java/com/example/pipelinetestforjava/PipelineTestForJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipelinetestforjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineTestForJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineTestForJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pipelinetestforjava/PipelineTestForJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelinetestforjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineTestForJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
